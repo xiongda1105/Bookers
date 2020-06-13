f5520fb04a3c87a29c238f246f091cf30be02075
vagrant <vagrant@localhost.localdomain>
2020-06-13 05:40:00 +0000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/edit'
+  get 'books/show'
+  get 'homes/top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
